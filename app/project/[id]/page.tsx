dbb1804a686aaea2f4adeb528549b65e8a22cefe
Andrei Tudorache <tudoracheandrei09@gmail.com>
2023-07-06 21:12:39 +0300

    Project page basics

---
+++ app/project/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { ProjectInterface } from '@/common.types'
+import { getProjectDetails } from '@/lib/actions'
+import { getCurrentUser } from '@/lib/session'
+import React from 'react'
+
+const Project = async ({ params: { id }}: { params: {id : string }}) => {
+  const session = await getCurrentUser()
+  const result = await getProjectDetails(id) as {project?: ProjectInterface}
+
+  if(!result?.project) {
+      return <p>Failed to find project</p>
+  }
+
+  console.log(result?.project)
+
+  return (
+    <div>page</div>
+  )
+}
+
+export default Project
